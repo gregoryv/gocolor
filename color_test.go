d50075c82d892afab22a409f983453cfee585e39
Gregory Vincic <g@7de.se>
2022-03-24 21:32:13 +0100

    Use larger dataset for benchmark

--- color_test.go
+++ color_test.go
@@ -4,13 +4,25 @@ import (
 	"bytes"
 	"errors"
 	"io/ioutil"
+	"os"
 	"strings"
 	"testing"
 )
 
 func TestColorize(t *testing.T) {
 	var buf bytes.Buffer
+	const testOutput = `=== RUN   TestSomething
+--- PASS:   TestSomething
+    --- PASS:
+--- FAIL:
+--- SKIP:
 
+a
+b
+PASS
+FAIL
+
+`
 	r := strings.NewReader(testOutput)
 	err := Colorize(&buf, r)
 	t.Log("painting input")
@@ -34,22 +46,14 @@ func TestNothing(t *testing.T) {
 }
 
 func BenchmarkColorize(b *testing.B) {
-	r := strings.NewReader(testOutput)
+	data, err := os.ReadFile("testdata/test.output")
+	if err != nil {
+		b.Fatal(err)
+	}
+	r := bytes.NewReader(data)
+	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		Colorize(ioutil.Discard, r)
-		r.Reset(testOutput)
+		r.Reset(data)
 	}
 }
-
-const testOutput = `=== RUN   TestSomething
---- PASS:   TestSomething
-    --- PASS:
---- FAIL:
---- SKIP:
-
-a
-b
-PASS
-FAIL
-
-`
