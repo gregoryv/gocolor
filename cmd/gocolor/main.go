39b13003c908a927eb6c58c4ec9e946d92112e50
Gregory Vincic <gregoryvse@gmail.com>
2022-11-27 07:53:35 +0100

    Use gregoryv/cmdline

--- cmd/gocolor/main.go
+++ cmd/gocolor/main.go
@@ -1,19 +1,17 @@
 package main
 
 import (
-	"flag"
-	"fmt"
 	"os"
 
+	"github.com/gregoryv/cmdline"
 	"github.com/gregoryv/gocolor"
 )
 
 func main() {
-	flag.Usage = func() {
-		fmt.Fprint(os.Stderr, `Usage: go test . | gocolor
-`)
-	}
-	flag.Parse()
+	var (
+		cli = cmdline.NewBasicParser()
+	)
+	cli.Parse()
 	err := gocolor.Colorize(os.Stdout, os.Stdin)
 	if err != nil {
 		os.Exit(1)
--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/gregoryv/gocolor
 
 go 1.17
 
-require github.com/gregoryv/vt100 v0.2.0 // indirect
+require (
+	github.com/gregoryv/cmdline v0.12.0
+	github.com/gregoryv/vt100 v0.2.0
+)
+
+require github.com/gregoryv/nexus v0.5.1 // indirect
